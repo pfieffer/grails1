cf5cb132f979504b580b674b093bac1ddef072b5
RaviGarbujaPun <cbpmgr@gmail.com>
2017-07-31 14:23:34 +0545

    Create domain class Vehicle

---
+++ grails-app/domain/myapp/Vehicle.groovy
@@ -0,0 +1,15 @@
+package myapp
+
+class Vehicle {
+
+	String name
+
+	String make
+	String model
+
+    static constraints = {
+    	name maxSize: 255
+    	make inList: ['Ford', 'Chevrolet', 'Nissan']
+    	model nullable: true
+    }
+}
